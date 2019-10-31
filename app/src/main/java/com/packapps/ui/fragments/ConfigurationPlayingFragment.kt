1fa430d58862e243c1cd3e766c7c8fae6230dd03
Paulo Linhares <plinhares9@gmail.com>
2019-10-31 17:17:40 -0300

    activeted is ok

--- app/src/main/java/com/packapps/ui/fragments/ConfigurationPlayingFragment.kt
+++ app/src/main/java/com/packapps/ui/fragments/ConfigurationPlayingFragment.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 
 import com.packapps.R
+import kotlinx.android.synthetic.main.fragment_configuration_playing.*
 import kotlinx.android.synthetic.main.fragment_configuration_playing.view.*
 
 class ConfigurationPlayingFragment : DialogFragment() {
@@ -28,6 +29,17 @@ class ConfigurationPlayingFragment : DialogFragment() {
         }
         mView.btApply.setOnClickListener {
             dismiss()
+        }
+
+        //how much times
+        mView.bt1x.isActivated = true
+        mView.bt1x.setOnClickListener {
+            mView.bt1x.isActivated = !bt1x.isActivated
+            mView.bt2x.isActivated = false
+        }
+        mView.bt2x.setOnClickListener {
+            mView.bt1x.isActivated = false
+            mView.bt2x.isActivated = !bt2x.isActivated
         }
 
         return mView
